d2a376eb51beb926313f1a5c4e50842908bfea59
Alexander Komshanov <a.komshanovjr@gmail.com>
2020-03-10 02:01:18 +0300

    Задание 3

--- Task3/a-koshmarov/metrics.py
+++ Task3/a-koshmarov/metrics.py
@@ -10,4 +10,25 @@ def binary_classification_metrics(prediction, ground_truth):
 
 
 def multiclass_accuracy(prediction, ground_truth):
-    return 0
+    """
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    """
+
+    correct = 0
+
+    for i in range(ground_truth.shape[0]):
+        if prediction[i] == ground_truth[i]:
+            correct += 1
+
+    if prediction.shape[0] != 0:
+        accuracy = correct / prediction.shape[0]
+    else:
+        accuracy = 0
+    return accuracy
